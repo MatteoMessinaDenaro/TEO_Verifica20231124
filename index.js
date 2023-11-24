f4851efe2b3c547314b2b0906d684fb7bfa0eafd
MatteoMessinaDenaro <l.marengo.2142@vallauri.edu>
2023-11-24 10:47:30 +0100

    Verifica Marengo
    Verifica Di Marengo

--- index.js
+++ index.js
@@ -1,23 +1,148 @@
+
+
 //POTETE AGGIUNGERE VARIABILI GLOBALI
+var ObjJSON = [];
+var prodottiSalvati = [];
+var prod = {};
+var dataChart = [];
 
 //DA COMPLETARE (Connettersi al server chiedendo dei singoli servizi)
-window.onload = ()=>{
+window.onload = async ()=>{
 
+    // Prelevo File JSON
+
+    await getDati();
+
+    caricaSelect();
+
+}
+
+async function getDati()
+{
+    await fetch("dati.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log("Dati ricevuti: ", data);
+
+        ObjJSON = data;
+
+    })
+    .catch((err) => {
+        console.error("Error: ", err);
+    })
 }
 
 //DA COMPLETARE 
-function caricaSelect(select, vet){
+function caricaSelect(select, vet){ 
+    // Carico Utente
 
+    let selectUtente = document.getElementById("selUt");
+
+    for (let i = 0; i < 10; i++)
+    {
+        let option = document.createElement("option");
+        option.textContent = i;
+        selectUtente.appendChild(option);
+    }  
+
+    // Carica Negozi
+
+    let selectNegozio = document.getElementById("selNegozio");
+    let negoziAggiunti = [];
+
+    ObjJSON.forEach(element => {
+
+        if (!negoziAggiunti.includes(element.negozio))
+        {
+            let option = document.createElement("option");
+            option.textContent = element.negozio;
+            selectNegozio.appendChild(option);
+
+            negoziAggiunti.push(element.negozio);
+        }
+    });
+
+    let selectProdotto = document.getElementById("selProdotto");
+    let prodottiAggiunti = [];
+    let products = [];
+
+    ObjJSON.forEach(element => {
+
+        products.push(element.prodotti);    
+    });
+
+    console.log(products);
+
+    products.forEach(element => {
+        
+        prodottiAggiunti.push(element.nome);
+        // }
+        for (let i = 0; i < element.length; i++)
+        {
+            if (!prodottiAggiunti.includes(element[i].nome))
+            {
+                let option2 = document.createElement("option");
+                option2.textContent = element[i].nome;
+                selectProdotto.appendChild(option2);
+
+                prodottiAggiunti.push(element[i].nome);
+            }
+        }
+    })
+
+    let btnSalvaProd = document.getElementById("btnSalvaProd");
+
+    btnSalvaProd.addEventListener("click", () => {
+        salvaProdotto(prodottiSalvati);
+    });
+
+    let btnSalvaSpesa = document.getElementById("btnSalvaSpesa");
+
+    btnSalvaSpesa.addEventListener("click", () => {
+        salvaSpesa(prodottiSalvati);
+    });
+    
 }
 
 //DA COMPLETARE e RICHIAMARE IN MODO OPPORTUNO
-function salvaProdotto(){
+function salvaProdotto(prodSalvati){
 
+    let selectProdotto = document.getElementById("selProdotto");
+    let prodotto = selectProdotto.value;
+    let quantity = document.getElementById("txtQta").value;
+
+    let prod = {"nome": prodotto, "quantità": quantity};
+
+    prodSalvati.push(prod);
+    console.log(prodSalvati);
 }
 
+
 //DA COMPLETARE e RICHIAMARE IN MODO OPPORTUNO
-function salvaSpesa(){
-  
+async function salvaSpesa(Produces){
+    
+    let user = document.getElementById("selUt").value;
+    let dateTime = document.getElementById("txtData").value;
+    let nomeNegozio = document.getElementById("selNegozio").value;
+    let impSpesa = document.getElementById("txtImporto").value;
+
+    let values = {
+        "user": user,
+        "dateTime": dateTime,
+        "nomeNegozio": nomeNegozio,
+        "impSpesa": impSpesa,
+        "prodotti": Produces
+    };
+
+
+    let container = await fetch("http://localhost:1337/saveData", {
+        "method": "POST",
+        "headers": {"Content-Type": "application/json"},
+        "body": JSON.stringify(values)
+    });
+
+    let response = await container.json();
+    console.log("Risposta dal Server: ", response);
 }
 
 //POTETE INSERIRE ALTRE FUNZIONI
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //LEGGO IL FILE e LO FILTRO SECONDO IL SERVIZIO
 
 //SERVIZI OBBLIGATORI DA ESPORRE (Sono per lo più contatori!)
-  //1. Spese effettuate in generale e numero di prodotti comprati per ogni spesa
+  //1. Spese effettuate in generale e numero di prodotti comprati per ogni spesa (grafico con asse x data e asse y quantità)
       
   //2. I prodotti comprati dall'utente 3 e quante volte in totale 
 
@@ -14,3 +14,102 @@
   //6. Indicare per ogni mese quanto si è speso
 
   //7. Salvataggio di una nuova spesa NEL FILE
+
+const { info } = require("console");
+const http = require("http");
+const url = require("url");
+
+const fs = require("fs");
+
+var server = http.createServer(function (request, response) {
+
+    let address = request.headers.host + request.url;
+    let infoURL = url.parse(address, true);
+    let header, file;
+
+    switch(infoURL.pathname)
+    {
+        case "/index":
+
+            contesto = {risp: response, type: "text/html"};
+            fs.readFile("index.html", inviaFile.bind(contesto));
+
+            break;
+
+        case "/index.css":
+
+            contesto = {risp: response, type: "text/css"};
+            fs.readFile("index.css", inviaFile.bind(contesto));
+
+            break;
+
+        case "/index.js":
+
+            contesto = {risp: response, type: "text/javascript"};
+            fs.readFile("index.js", inviaFile.bind(contesto));
+
+            break;
+
+        case "/dati.json":
+
+            contesto = {risp: response, type: "text/json"};
+            fs.readFile("dati.json", inviaFile.bind(contesto));
+
+            break;
+
+        case "/saveData":
+
+             // Raccolgo il Body della Richiesta
+
+             let body = "";
+
+             // Intercetto l'arrivo di un pezzetto di Body
+ 
+             request.on("data", (dato) => {
+                 console.log("Data: ", dato);
+                 body += dato;
+             });
+ 
+             // Intercetta la fine della comunicazione
+             
+             request.on("end", () => {
+                 console.log(body);
+                 fs.writeFile("nuovaSpesaUtente.json", JSON.stringify(body), (err) => {
+                     if (err) {
+                         response.writeHead(500, {"Content-Type": "application/json"});
+                         let json = {desc: "Errore nel Salvataggio"};
+                         response.write(JSON.stringify(json));
+                         response.end();
+                     }
+                     else {
+                         response.writeHead(200, {"Content-Type": "application/json"});
+                         let json = {desc: "Salvataggio avvenuto con Successo!"};
+                         response.write(JSON.stringify(json));
+                         response.end();
+                     }
+                 });
+             })
+
+            break;
+  
+        default:
+            header = {"Content-Type": "text/plain"};
+            response.writeHead(404, header);
+            response.write("Nessuna risorsa trovata!");
+            response.end();
+            break;
+    }
+})
+
+server.listen(1337);
+console.log("Il server è avviato sulla porta 1337...");
+
+function inviaFile(err, file)
+{
+    console.log(this);
+
+    let header = {"Content-Type": this.type};
+    this.risp.writeHead(200, header);
+    this.risp.write(file);
+    this.risp.end();
+}
